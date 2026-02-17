972fb315c1ae17dd28546190f9467e8b05d0de65
unknown <lukpet591@gmail.com>
2026-02-17 03:33:30 +0100

    velike promene u stilu i dodat francuski jezik push

--- src/komponente/Galerija2.jsx
+++ src/komponente/Galerija2.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { Modal, Button } from 'react-bootstrap';
 import { motion, AnimatePresence } from 'framer-motion';
 
 // Putanje ka slikama unutar 'src/assets'
